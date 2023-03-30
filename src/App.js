aba67b7a8ddfdc9b588cd8cc8fe503510926541e
jolak5 <kayodej4@gmail.com>
2023-03-30 10:44:26 +0100

    create linters

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+/* eslint-disable import/no-extraneous-dependencies */
 import './App.css';
+import {
+  createBrowserRouter, Route, createRoutesFromElements, RouterProvider,
+} from 'react-router-dom';
+import NavBar from './components/navBar';
+import Books from './components/books';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<NavBar />}>
+      <Route index element={<Books />} />
+    </Route>,
+  ),
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
