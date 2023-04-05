23bedadf5a2f945707ca655486178734013cddb3
jolak5 <kayodej4@gmail.com>
2023-04-05 08:21:01 +0100

    Implement reviewers suggestions

--- src/components/redux/books/booksSlice.js
+++ src/components/redux/books/booksSlice.js
@@ -9,7 +9,7 @@ export const bookSlice = createSlice({
         id: 'item1',
         title: 'The Great Gatsby',
         author: 'John Smith',
-        category: 'Fictions',
+        category: 'Fiction',
       },
       {
         id: 'item2',
