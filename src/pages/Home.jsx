6dd3150bb8e0d01c5ed66e68e046e81c7f34d04a
jolak5 <kayodej4@gmail.com>
2023-04-07 11:56:51 +0100

    fix the progress bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import { useState } from 'react';
 import Form from '../components/Form';
+import 'react-circular-progressbar/dist/styles.css';
 import './home.css';
 import { deleteBook, deleteAPIBook } from '../components/redux/books/booksSlice';
 
@@ -7,7 +10,12 @@ import { deleteBook, deleteAPIBook } from '../components/redux/books/booksSlice'
 export default function Home() {
   const dispatch = useDispatch();
   const books = useSelector((state) => state.book.books);
+  const [percentage, newPercentage] = useState(65);
 
+  const handlePercentage = (e) => {
+    e.preventDefault();
+    newPercentage(Math.floor(Math.random() * 100));
+  };
   return (
     <div className="bookList">
       { (books ?? []).filter((b) => b.item_id).map((book) => (
@@ -27,16 +35,35 @@ export default function Home() {
             </div>
           </div>
           <div className="middle-pan">
-            <div className="circle" />
+            <div style={{ width: '100px', height: '100px' }}>
+              <CircularProgressbar
+                value={percentage}
+                strokeWidth={8}
+                styles={{
+                  path: {
+                    stroke: '#3f51b5',
+                    strokeLinecap: 'butt',
+                    transition: 'stroke-dashoffset 0.5s ease 0s',
+                  },
+                  trail: {
+                    stroke: '#d6d6d6',
+                    strokeLinecap: 'butt',
+                  },
+                }}
+              />
+            </div>
             <div>
-              <h1 className="percentage">64%</h1>
+              <h1 className="percentage">
+                {percentage}
+                %
+              </h1>
               <p>Completed</p>
             </div>
           </div>
           <div className="right-pan">
             <p>CURRENT CHAPTER</p>
             <h5>Chapter 17</h5>
-            <button type="button" className="updateProgress">UPDATE PROGRESS</button>
+            <button type="button" className="updateProgress" onClick={handlePercentage}>UPDATE PROGRESS</button>
           </div>
         </div>
 
